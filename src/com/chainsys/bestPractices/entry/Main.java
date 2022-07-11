3532ac9ef7ff81a32754969b732757ad71e319b3
NaveenRomi <naveenromi27@gmail.com>
2022-07-11 17:41:49 +0530

    updating files

--- src/com/chainsys/bestPractices/entry/Main.java
+++ src/com/chainsys/bestPractices/entry/Main.java
@@ -3,6 +3,13 @@ package com.chainsys.bestPractices.entry;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Boomi");
+		String s1="Chainsys";
+		String s2="Chainsys";
+		String s3="Chain"+"sys";
+		String s4="chainsys";
+		System.out.println(s1.hashCode());
+		System.out.println(s2.hashCode());
+		System.out.println(s3.hashCode());
+		System.out.println(s4.hashCode());
 	}
 }
